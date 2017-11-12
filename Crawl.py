b02b21b3722a8d2ac84ad4e5af573f790e23b2b2
Anshuman Dey Kirty <anshuman.kirty@gmail.com>
2017-11-12 19:08:06 +0530

    Add files via upload

---
+++ Crawl.py
@@ -0,0 +1,38 @@
+# @author Anshuman
+# Basic Crawler
+import urllib.request
+import timeit
+import webbrowser
+def findurls( html,count):
+    for look in html:
+        if "<a href=" in look and "http://" in look:
+            start=look.find("http://")
+            end=look.find("\"",start)
+            urllist.append(look[start:end])
+            print(count)
+            print("\t")
+    maxpage=500
+    while len(urllist)< maxpage and count < len(urllist)-1:
+        try:
+            local_filename1, headers1 = urllib.request.urlretrieve(urllist[count])
+            count+=1
+            html1=open(local_filename1,encoding="utf8")     
+            findurls(html1,count)
+            print(count)
+            print("\n")
+        except:
+            break
+        
+urllist=[]
+start=timeit.default_timer()
+local_filename, headers = urllib.request.urlretrieve('http://www.iitb.ac.in/')
+html = open(local_filename,encoding="utf8")
+count=0
+findurls (html,count);
+stop=timeit.default_timer()
+urllist=list(set(urllist))
+print (urllist)
+print (len(urllist))
+print (stop-start)
+
+    
